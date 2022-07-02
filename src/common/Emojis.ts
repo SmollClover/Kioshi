168aa05b7941c79eb5cb0489636281d47f2d0ed4
Clover <39840298+SmollClover@users.noreply.github.com>
2022-07-02 16:04:10 +0200

    [Emojis] Added more Emojis

--- src/common/Emojis.ts
+++ src/common/Emojis.ts
@@ -1,5 +1,9 @@
 export const Emojis = {
 	done: '<:done:827250778447937566>',
+	error: '<:error:827250778477559859>',
+	remove: '<:remove:827250778330497034>',
+	add: '<:add:827250778318045244>',
+	edit: '<:edit:827253250041577483>',
 	question_mark: '<:question_mark:992767461843812423>',
 	shield: '<:shield:992768390106189864>',
 	key: '<:key:992767464150679583>',
