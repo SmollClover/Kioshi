c702d40e008f60370e8074186b042215dc9bbb16
Clover <39840298+SmollClover@users.noreply.github.com>
2022-07-02 22:47:10 +0200

    [VerifyIntegrity] Forced Name, Limit and Permissions

--- src/intervals/Channels/VerifyIntegrityInterval.ts
+++ src/intervals/Channels/VerifyIntegrityInterval.ts
@@ -11,14 +11,62 @@ export const run: RunFunction = async (client) => {
 
 	await Promise.all(
 		Settings.map(async (Setting) => {
-			const Entries = (await DataSchema.findOne({ Guild: Setting.Guild })) as Data[];
+			const Entries = (await DataSchema.find({ Guild: Setting.Guild })) as Data[];
 
 			await Promise.all(
 				Entries.map(async (Data) => {
 					if ((await ensureChannel(client, Setting, Data)) === true) return;
 
+					const Guild = await getGuild(client, Data.Guild);
+					if (!Guild) return;
 					const Channel = await getChannel(client, Data.Guild, Data.Channel);
 					if (!Channel) return;
+					const Member = await getMember(client, Data.Guild, Data.User);
+					if (!Member) return;
+
+					if (Channel.userLimit !== Data.Limit) await Channel.setUserLimit(Data.Limit);
+					if (Channel.name !== Data.Name) {
+						if (Data.Name) await Channel.setName(Data.Name);
+						if (!Data.Name && Channel.name !== Member.user.username) await Channel.setName(Member.user.username);
+					}
+
+					Channel.permissionOverwrites.cache.map((perm) => {
+						if (perm.id === Guild.roles.everyone.id) {
+							if (Channel.permissionsFor(perm.id).has('VIEW_CHANNEL', false) && Data.Private) {
+								Channel.permissionOverwrites.edit(perm.id, { VIEW_CHANNEL: false });
+							} else if (!Channel.permissionsFor(perm.id).has('VIEW_CHANNEL', false) && !Data.Private) {
+								Channel.permissionOverwrites.edit(perm.id, { VIEW_CHANNEL: true });
+							}
+
+							return;
+						}
+
+						if (Channel.permissionsFor(perm.id).has('MANAGE_MESSAGES', false)) {
+							if (!Setting.Moderators.includes(perm.id)) Channel.permissionOverwrites.delete(perm.id);
+						}
+
+						if (Setting.Moderators.includes(perm.id)) {
+							if (!Channel.permissionsFor(perm.id).has('MANAGE_MESSAGES', false))
+								Channel.permissionOverwrites.create(perm.id, {
+									VIEW_CHANNEL: true,
+									MANAGE_MESSAGES: true,
+									MENTION_EVERYONE: true,
+									USE_APPLICATION_COMMANDS: true,
+									MOVE_MEMBERS: true,
+									MUTE_MEMBERS: true,
+								});
+						}
+
+						if (Channel.permissionsFor(perm.id).has('VIEW_CHANNEL', false)) {
+							if (!Setting.Moderators.includes(perm.id) && !Data.AddedUsers.includes(perm.id))
+								Channel.permissionOverwrites.delete(perm.id);
+						}
+
+						if (Data.AddedUsers.includes(perm.id)) {
+							if (!Channel.permissionsFor(perm.id).has('VIEW_CHANNEL', false))
+								Channel.permissionOverwrites.create(perm.id, { VIEW_CHANNEL: true });
+						}
+					});
 				})
 			);
 		})
@@ -115,4 +163,4 @@ async function ensureChannel(client: client, Settings: Settings, Data: Data): Pr
 }
 
 export const name = 'verifyIntegrity';
-export const milliseconds = 1000 * 60 * 30;
+export const milliseconds = 1000 * 60 * 60;
